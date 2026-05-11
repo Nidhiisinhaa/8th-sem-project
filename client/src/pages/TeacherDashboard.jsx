507caa4f67b68afd758c1c36633cc486485edcf3
yahhshika <soniyashika1619@gmail.com>
2026-05-11 23:22:48 +0530

    allowed copying2

--- client/src/pages/TeacherDashboard.jsx
+++ client/src/pages/TeacherDashboard.jsx
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import {
     Users, AlertTriangle, TrendingUp, Award,
     ChevronUp, Search, Filter
